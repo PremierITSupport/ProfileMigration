ad2b146ebe7fe9e836872cce43802fbafe1f48a6
Eric Stevenson <estevenson@premierag.com>
2021-08-26 16:32:52 -0400

    Update MigrationTool.ps1

--- MigrationTool.ps1
+++ MigrationTool.ps1
@@ -3,5 +3,6 @@ if (!(Test-Path $keyPath)) { New-Item $keyPath -Force | Out-Null }
 Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
 New-Item -ItemType Directory -Force -Path C:\Temp\ForensiT | Out-Null
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-WR2PREMIER.msi" -OutFile "C:\Temp\ForensiT\Migrate-WR2PREMIER.msi"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/TV/main/ProfileMigration.bat" -OutFile "C:\Temp\ForensiT\install.bat"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/ProfileMigration.bat" -OutFile "C:\Temp\ForensiT\install.bat"
 Start-Process cmd -argument "/c C:\Temp\ForensiT\install.bat"
+
