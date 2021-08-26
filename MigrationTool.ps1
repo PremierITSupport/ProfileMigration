30896e724694987c3265f6830411e79129d4960f
Eric Stevenson <estevenson@premierag.com>
2021-08-26 16:13:59 -0400

    Update MigrationTool.ps1

--- MigrationTool.ps1
+++ MigrationTool.ps1
@@ -1,7 +1,7 @@
 $keyPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Internet Explorer\Main'
 if (!(Test-Path $keyPath)) { New-Item $keyPath -Force | Out-Null }
 Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
-New-Item -ItemType Directory -Force -Path C:\Temp\TV | Out-Null
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-WR2PREMIER.exe" -OutFile "C:\Temp\ForensIt\Migrate-WR2PREMIER.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/TV/main/TV_local_generic.bat" -OutFile "C:\Temp\TV\install.bat"
-Start-Process cmd -argument "/c C:\Temp\TV\install.bat"
+New-Item -ItemType Directory -Force -Path C:\Temp\ForensiT | Out-Null
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-WR2PREMIER.msi" -OutFile "C:\Temp\ForensiT\Migrate-WR2PREMIER.msi"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/TV/main/ProfileMigration.bat" -OutFile "C:\Temp\ForensiT\install.bat"
+Start-Process cmd -argument "/c C:\Temp\ForensiT\install.bat"
