2cbe334b42e99234cf3dafbe448b8c50e11bdc29
Eric Stevenson <estevenson@premierag.com>
2021-08-27 11:18:57 -0400

    Update MigrationTool.ps1

--- MigrationTool.ps1
+++ MigrationTool.ps1
@@ -2,7 +2,7 @@ $keyPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Internet Ex
 if (!(Test-Path $keyPath)) { New-Item $keyPath -Force | Out-Null }
 Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
 New-Item -ItemType Directory -Force -Path C:\Temp\ForensiT | Out-Null
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-WR2PREMIER.msi" -OutFile "C:\Temp\ForensiT\Migrate-WR2PREMIER.msi"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-test27.exe" -OutFile "C:\Temp\ForensiT\Migrate-WR2PREMIER.msi"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/ProfileMigration.bat" -OutFile "C:\Temp\ForensiT\install.bat"
 Start-Process cmd -argument "/c C:\Temp\ForensiT\install.bat"
 
