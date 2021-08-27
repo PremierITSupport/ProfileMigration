8fa2df3c9e1f0944d942a6769471ab016a0db458
Eric Stevenson <estevenson@premierag.com>
2021-08-27 12:01:02 -0400

    Update MigrationTool.ps1

--- MigrationTool.ps1
+++ MigrationTool.ps1
@@ -3,5 +3,5 @@ if (!(Test-Path $keyPath)) { New-Item $keyPath -Force | Out-Null }
 Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
 New-Item -ItemType Directory -Force -Path C:\Temp\Migrate | Out-Null
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/ProfileMigration0.bat" -OutFile "C:\Temp\Migrate\install.bat"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-test27.exe" -OutFile "C:\Temp\Migrate\Migrate-test27.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PremierITSupport/ProfileMigration/main/Migrate-test27.exe" -OutFile "C:\Temp\Migrate\Migrate-test27.msi"
 Start-Process cmd -argument "/c C:\Temp\Migrate\install.bat"
